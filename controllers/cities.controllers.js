5a22f3593ea67038f4bef6f5af245da3218a98d8
Melody Flores <melody03@hotmail.com.ar>
2023-08-21 20:27:04 -0300

    queries and cities.json created

--- controllers/cities.controllers.js
+++ controllers/cities.controllers.js
@@ -1,9 +1,51 @@
+import Cities from "../models/Cities.js"
+
 const controller = {
-    getCities: () => {
+    getCities: async (req, res) => {
 
+        let queries = {}
+
+        if(req.query.name){
+            queries.name = req.query.name
+        }
+
+        try{
+            //objeto del find es el FILTRO
+            const city = await Cities.find(queries) //mongoose lo usa con un modelo
+
+            return res.status(200).json({
+                succes: true,
+                city
+            })
+        }catch(error){
+            console.log(error);
+            return res.status(500).json({
+                success: false,
+                message: "Error while getting city"
+            })
+        }
     },
-    createCities: ()=> {
+    createCities: async (req, res)=> {
+        try{
+            const newCity = await Cities.create(req.body); 
+            return res.status(201).json({
+                success: true,
+                message: "No errors!"
+            })
 
+        } catch(error) {
+            res.status(500).json({
+                success: false,
+                message: "Error while creating city"
+            })
+        }
+        
+        //create recibe un objeto q voy a cargar a mi base de datos
+        
+        return res.status(201).json({
+            success: true, 
+            message: 'City created'
+        })
     }
 }
 
--- models/Cities.js
+++ models/Cities.js
@@ -1,11 +1,11 @@
-import { Schema, model, types } from "mongoose";
+import { Schema, model, Types } from "mongoose";
 
 let collections = 'Cities';
 let schema = new Schema({ //propiedades
-    name: {type: String,required:true},
-    province: {type: String, require:true},
+    city: {type: String,required:true},
+    image: {type: String, require:true},
     description: { type: String, require:true},
-    image: { type: String, require:true},
+    province: { type: String, require:true},
 }, {
     timestamps: true //fechas
 })
